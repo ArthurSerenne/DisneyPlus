a19421bbfe30fe56ea9c22ce2dba90ad85822413
ArthurSerenne <79085879+ArthurSerenne@users.noreply.github.com>
2021-05-03 17:24:17 +0200

    up

--- index.php
+++ index.php
@@ -17,45 +17,61 @@
 	?>
 
 	<section id="carouselAccueil">
-		<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-			<div class="carousel-inner">
-				<div class="carousel-item active">
-					<img class="d-block w-100" src="images/Accueil/Diaporama/1.jpg" alt="First slide">
+		<div class="container-fluid">
+			<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+				<div class="carousel-inner">
+					<div class="carousel-item active">
+						<img class="d-block w-100" src="images/Accueil/Diaporama/1.jpg" alt="First slide">
+					</div>
+					<div class="carousel-item active">
+						<img class="d-block w-100" src="images/Accueil/Diaporama/1.png" alt="Second slide">
+					</div>
 				</div>
-				<div class="carousel-item active">
-					<img class="d-block w-100" src="images/Accueil/Diaporama/1.png" alt="Second slide">
-				</div>
+				<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="sr-only">Previous</span>
+				</a>
+				<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="sr-only">Next</span>
+				</a>
 			</div>
-			<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-				<span class="sr-only">Previous</span>
-			</a>
-			<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-				<span class="carousel-control-next-icon" aria-hidden="true"></span>
-				<span class="sr-only">Next</span>
-			</a>
 		</div>
 	</section>
 
 	<section id="listeMarque">
 		<div class="container-fluid">
-			<div class="iconMarque">
-				<img class="icon1" src="images/Accueil/Marques/1.png">
-			</div>
-			<div class="iconMarque">
-				<img class="icon1" src="images/Accueil/Marques/2.png">
-			</div>
-			<div class="iconMarque">
-				<img class="icon1" src="images/Accueil/Marques/3.png">
-			</div>
-			<div class="iconMarque">
-				<img class="icon1" src="images/Accueil/Marques/4.png">
-			</div>
-			<div class="iconMarque">
-				<img class="icon1" src="images/Accueil/Marques/5.png">
-			</div>
-			<div class="iconMarque">
-				<img class="icon1" src="images/Accueil/Marques/6.png">
+			<div class="row">
+				<div class="col-xs-6 col-sm-4 col-md-2">
+					<div class="iconMarque1">
+						<img class="icon1" src="images/Accueil/Marques/1.png">
+					</div>
+				</div>
+				<div class="col-xs-6 col-sm-4 col-md-2">
+					<div class="iconMarque2">
+						<img class="icon1" src="images/Accueil/Marques/2.png">
+					</div>
+				</div>
+				<div class="col-xs-6 col-sm-4 col-md-2">
+					<div class="iconMarque3">
+						<img class="icon1" src="images/Accueil/Marques/3.png">
+					</div>
+				</div>
+				<div class="col-xs-6 col-sm-4 col-md-2">
+					<div class="iconMarque4">
+						<img class="icon1" src="images/Accueil/Marques/4.png">
+					</div>
+				</div>
+				<div class="col-xs-6 col-sm-4 col-md-2">
+					<div class="iconMarque5">
+						<img class="icon1" src="images/Accueil/Marques/5.png">
+					</div>
+				</div>
+				<div class="col-xs-6 col-sm-4 col-md-2">
+					<div class="iconMarque6">
+						<img class="icon1" src="images/Accueil/Marques/6.png">
+					</div>
+				</div>
 			</div>
 		</div>
 	</section>
@@ -72,6 +88,36 @@
 			</div>
 		</div>
 	</section>
+
+	<section id="ligne2">
+		<div class="container-fluid">
+			<h3>Nouveau sur Disney+</h3>
+			<div class="premiereLigne">
+				<img class="imageLigne2" src="images/Accueil/Ligne2/1.jpg">
+				<img class="imageLigne2" src="images/Accueil/Ligne2/2.jpg">
+				<img class="imageLigne2" src="images/Accueil/Ligne2/3.jpg">
+				<img class="imageLigne2" src="images/Accueil/Ligne2/4.jpg">
+				<img class="imageLigne2" src="images/Accueil/Ligne2/5.jpg">
+			</div>
+		</div>
+	</section>
+
+	<section id="ligne3">
+		<div class="container-fluid">
+			<h3>Nouveau sur Disney+</h3>
+			<div class="premiereLigne">
+				<img class="imageLigne3" src="images/Accueil/Ligne3/1.jpg">
+				<img class="imageLigne3" src="images/Accueil/Ligne3/2.jpg">
+				<img class="imageLigne3" src="images/Accueil/Ligne3/3.jpg">
+				<img class="imageLigne3" src="images/Accueil/Ligne3/4.jpg">
+				<img class="imageLigne3" src="images/Accueil/Ligne3/5.jpg">
+			</div>
+		</div>
+	</section>
+
+	<?php
+	require("footer.php")
+	?>
 
 </body>
 </html>
